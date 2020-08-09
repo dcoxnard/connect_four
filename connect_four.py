054fc2f65a1296a2d8ff5b6eadbc61e86937177f
dcoxnard <dcoxnard89@gmail.com>
2020-08-09 10:46:50 -0400

    Some first unit tests and stub functions

---
+++ connect_four.py
@@ -0,0 +1,28 @@
+#
+# Code to play a game of Connect Four
+#
+
+
+def simple_test():
+    return("test result")
+
+
+def create_board():
+    # Code here
+    raise NotImplementedError
+
+
+def place_token(board, token_index, token):
+    raise NotImplementedError
+
+
+def find_vertical_win(board, token):
+    raise NotImplementedError
+
+
+def find_horizontal_win(board, token):
+    raise NotImplementedError
+
+
+def find_diagonal_win(board, token):
+    raise NotImplementedError
---
+++ tests.py
@@ -0,0 +1,77 @@
+import unittest
+import connect_four as cf
+
+
+class TestConnectFour(unittest.TestCase):
+
+    def test_ping(self):
+        result = cf.simple_test()
+        self.assertEqual(result, "test result")
+
+    def test_create_board(self):
+        board = cf.create_board()
+        expected_board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+        self.assertEqual(board, expected_board)
+
+    def test_place_token(self):
+        input_board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+        token_index = 3
+        token = 1
+        expected_output_board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+        ]
+        output_board = cf.place_token(input_board, token_index, token)
+        self.assertEqual(output_board, expected_output_board)
+
+    def test_find_vertical_win(self):
+        # Positive case
+        board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+        ]
+        token = 1
+        expected_output = True
+        win_found = cf.find_vertical_win(board, token)
+        self.assertEqual(win_found, expected_output)
+
+        # Negative case
+        board = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 2, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+        ]
+        token = 1
+        expected_output = False
+        win_found = cf.find_vertical_win(board, token)
+        self.assertEqual(win_found, expected_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
